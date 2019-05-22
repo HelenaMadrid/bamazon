5beec02a4113a5b32a9e189ee5fa38aef6c79277
HelenaMadrid <helena.madrid@directrips.com>
2019-05-22 15:08:57 -0500

    finished add new product and view low inventory

--- bamazonManager.js
+++ bamazonManager.js
@@ -33,42 +33,104 @@ function start() {
                 choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product", "EXIT"]
             }
         ])
-        .then(function(answers){
-            if(answers.selectAction=== "View Products for Sale"){
+        .then(function (answers) {
+            if (answers.selectAction === "View Products for Sale") {
                 viewProducts();
             }
-            else if(answers.selectAction==="View Low Inventory"){
+            else if (answers.selectAction === "View Low Inventory") {
                 viewLowInventory();
             }
-            else if(answers.selectAction==="Add to Inventory"){
+            else if (answers.selectAction === "Add to Inventory") {
                 addInventory();
             }
-            else if(answers.selectAction==="Add New Product"){
+            else if (answers.selectAction === "Add New Product") {
                 addNewProduct();
             }
-            else{
+            else {
                 connection.end();
             }
         });
 }
-function viewProducts(){
+function viewProducts() {
     connection.query("SELECT * FROM products", function (err, results) {
         if (err) throw err;
+        console.log("\n---------------------------------------------------\n");
         //console.log(results);
         for (var x = 0; x < results.length; x++) {
             console.log(results[x].item_id + " | " + results[x].product_name + " | " + results[x].department_name + " | " + results[x].price + " | " + results[x].stock_quantity);
         }
+        console.log("\n---------------------------------------------------\n");
         start();
     })
 }
 
-function viewLowInventory(){
-
+function viewLowInventory() {
+    connection.query("SELECT * FROM products WHERE stock_quantity < 5", function (err, results) {
+        if (err) throw err;
+        console.log("\n---------------------------------------------------\n");
+        //console.log(results);
+        for (var x = 0; x < results.length; x++) {
+            console.log(results[x].item_id + " | " + results[x].product_name + " | " + results[x].department_name + " | " + results[x].price + " | " + results[x].stock_quantity);
+        }
+        console.log("\n---------------------------------------------------\n");
+        start();
+    })
 }
 
-function addInventory(){
+function addInventory() {
 
 }
-function addNewProduct(){
-    
+function addNewProduct() {
+    inquirer
+        .prompt([
+            {
+                name: "productName",
+                type: "input",
+                message: "What is the name of the product?"
+            },
+            {
+                name: "departmentName",
+                type: "input",
+                message: "What department does this product belong to?"
+            },
+            {
+                name: "unitPrice",
+                type: "input",
+                message: "What is the unit price of the product?",
+                validate: function (value) {
+                    if (isNaN(value) === false) {
+                        return true;
+                    }
+                    return false;
+                }
+            },
+            {
+                name: "stock",
+                type: "input",
+                message: "How much of this product do you have?",
+                validate: function (value) {
+                    if (isNaN(value) === false) {
+                        return true;
+                    }
+                    return false;
+                }
+            }
+        ])
+        .then(function (answers) {
+            connection.query("INSERT INTO products SET ?", {
+                product_name: answers.productName,
+                department_name: answers.departmentName,
+                price: answers.unitPrice,
+                stock_quantity: answers.stock
+
+            },
+                function (err) {
+                    if (err) throw err;
+                    console.log("Your product was created successfully!");
+                    // re-prompt the user for if they want to bid or post
+                    start();
+                }
+            )
+        });
+
 }
